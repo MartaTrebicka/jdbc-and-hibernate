4e6f59a5c2d79cf99238f599739e0fdf1d787ee2
Marta <marta.trebicka@wp.pl>
2023-03-04 13:49:43 +0100

    [change] - anonymous classes examples

--- src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
@@ -10,5 +10,14 @@ public class LambdaExercises {
 
         ChłopakiNiePłaczą boy2 = new ChłopakiNiePłaczą();
         boy2.sing();
+
+        // anonymous class
+        BoysDontCry anonymousClassReference = new BoysDontCry() { // stowrzyliśmy klasę bez nazwy która implementuje interface BoysDontCry
+            @Override
+            public void silnoreki() {
+                System.out.println("Anonimowy silnoreki");
+            }
+        };
+        anonymousClassReference.silnoreki();
     }
 }
