7b52ee17b6bb4747242c3900998845e837cb6f70
Marta <marta.trebicka@wp.pl>
2023-03-04 13:32:41 +0100

    [change] - interface with implementation - polimorphism

---
+++ src/main/java/pl/sda/orange2/lambda/ChłopakiNiePłaczą.java
@@ -0,0 +1,14 @@
+package pl.sda.orange2.lambda;
+
+public class ChłopakiNiePłaczą implements BoysDontCry {
+
+    @Override
+    public void silnoreki() {
+        System.out.println("Jestem Silnoręki");
+    }
+
+  public void sing() {  // metoda
+      System.out.println("Ruszaj się ......");
+
+  }
+}
--- src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
@@ -3,6 +3,12 @@ package pl.sda.orange2.lambda;
 public class LambdaExercises {
 
     public static void main(String[] args) {
+        BoysDontCry boy = new ChłopakiNiePłaczą();// obiekt
+        boy.silnoreki();
 
+        ((ChłopakiNiePłaczą)boy).sing();// rzutowanie
+
+        ChłopakiNiePłaczą boy2 = new ChłopakiNiePłaczą();
+        boy2.sing();
     }
 }
