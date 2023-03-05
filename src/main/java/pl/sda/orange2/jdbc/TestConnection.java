9a0402a7ce49e12a64f5305255c5b0df3ec69ebb
Marta <marta.trebicka@wp.pl>
2023-03-05 16:05:30 +0100

    [change] - test query example

--- src/main/java/pl/sda/orange2/jdbc/TestConnection.java
+++ src/main/java/pl/sda/orange2/jdbc/TestConnection.java
@@ -3,22 +3,46 @@ package pl.sda.orange2.jdbc;
 import pl.sda.orange2.jdbc.config.H2Config;
 
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TestConnection {
     public static void main(String[] args) {
 
+        String query = """
+                SELECT ID, NAME
+                FROM TEST
+                """;
+
+
         // DriverManager umożliwia stworzenie połaczenia do bazy danych.
         //Dzięki temu będziemy  mogli wysylac i odbierać zapytania
         try {
-            //var to skrót na typ.
+
+           // We need jdbc driver (h2 depedency at pom)
+            //connection gave us access to existing db
+            //var to skrót na typ.  taka rura do bazy. działa do zmiennych lokalnych
             var h2Connection = DriverManager.getConnection(H2Config.DB_URL,
                     H2Config.USER,
                     H2Config.PASSWORD);
 
             System.out.println("got connection: " + (h2Connection != null));
+
+        // Statement is used to send queries to db with existing connection
+        //statement to pojedyncze zapytanie wysłane w obrębie połączenia (rury do bazy danych?)
+       Statement queryStatement = h2Connection.createStatement();
+
+       //ResultSet contains query result data as simple table
+            //We need to iterate over result to got data
+    ResultSet queryResult =  queryStatement.executeQuery(query);
+    while (queryResult.next()) {
+        System.out.println("id: " + queryResult.getInt(1));
+        System.out.println("name: " + queryResult.getString(2));
+    }
         } catch (SQLException exc) {
             System.out.println("got exception: " + exc);
+            exc.printStackTrace();
         }
     }
 }
