157c33227d267a995785df4e033956e7a95cf187
Marta <marta.trebicka@wp.pl>
2023-03-07 20:26:00 +0100

    [fix] - closing database connection

--- src/main/java/pl/sda/orange2/jdbc/TestConnection.java
+++ src/main/java/pl/sda/orange2/jdbc/TestConnection.java
@@ -8,44 +8,105 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class TestConnection {
-    public static void main(String[] args) {
 
-       String className =  TestConnection.class.getName();
-        System.out.println("Current class name is " + className);
+
+    public static void main(String[] args) {
+        String className = TestConnection.class.getName();
+        System.out.println("Current class name is: " + className);
 
         String query = """
                 SELECT ID, NAME
                 FROM TEST
                 """;
 
-
-        // DriverManager umożliwia stworzenie połaczenia do bazy danych.
-        //Dzięki temu będziemy  mogli wysylac i odbierać zapytania
+        ResultSet queryResult = null;
         try {
-
-           // We need jdbc driver (h2 depedency at pom)
-            //connection gave us access to existing db
-            //var to skrót na typ.  taka rura do bazy. działa do zmiennych lokalnych
+//            Connection h2Connection = DriverManager.getConnection(H2Config.DB_URL,
+//                    H2Config.USER,
+//                    H2Config.PASSWORD);
+            // we need jdbc driver (h2 dependency at pom)
+            // connection gave us access to existing db
             var h2Connection = DriverManager.getConnection(H2Config.DB_URL,
                     H2Config.USER,
                     H2Config.PASSWORD);
-
             System.out.println("got connection: " + (h2Connection != null));
 
-        // Statement is used to send queries to db with existing connection
-        //statement to pojedyncze zapytanie wysłane w obrębie połączenia (rury do bazy danych?)
-       Statement queryStatement = h2Connection.createStatement();
+            // Statement is used to send queries to db with existing connection
+            Statement queryStatement = h2Connection.createStatement();
 
-       //ResultSet contains query result data as simple table
-            //We need to iterate over result to got data
-    ResultSet queryResult =  queryStatement.executeQuery(query);
-    while (queryResult.next()) {
-        System.out.println("id: " + queryResult.getInt(1));
-        System.out.println("name: " + queryResult.getString(2));
-    }
+            // ResultSet contains query result data as simple table
+            // we need to iterate over result to got data
+            queryResult = queryStatement.executeQuery(query);
+            while (queryResult.next()) {
+                System.out.println("id: " + queryResult.getInt(1));
+                System.out.println("name: " + queryResult.getString(2));
+            }
         } catch (SQLException exc) {
             System.out.println("got exception: " + exc);
             exc.printStackTrace();
+        } finally {
+            System.out.println("Finally I know it :)");
+            if (queryResult != null) {
+                try {
+                    queryResult.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
+
+        System.out.println("The end");
     }
 }
+
+//    public static void main(String[] args) {
+//
+//        String className = TestConnection.class.getName();
+//        System.out.println("Current class name is " + className);
+//
+//        String query = """
+//                SELECT ID, NAME
+//                FROM TEST
+//                """;
+//
+//        ResultSet queryStatement = null; // stworzenie (?)
+//        // DriverManager umożliwia stworzenie połaczenia do bazy danych.
+//        //Dzięki temu będziemy  mogli wysylac i odbierać zapytania
+//        try {
+//
+//            // We need jdbc driver (h2 depedency at pom)
+//            //connection gave us access to existing db
+//            //var to skrót na typ.  taka rura do bazy. działa do zmiennych lokalnych
+//            var h2Connection = DriverManager.getConnection(H2Config.DB_URL,
+//                    H2Config.USER,
+//                    H2Config.PASSWORD);
+//
+//            System.out.println("got connection: " + (h2Connection != null));
+//
+//            // Statement is used to send queries to db with existing connection
+//            //statement to pojedyncze zapytanie wysłane w obrębie połączenia (rury do bazy danych?)
+//           Statement queryStatement = h2Connection.createStatement();
+//
+//            //ResultSet contains query result data as simple table
+//            //We need to iterate over result to got data
+//            ResultSet queryResult = queryStatement.executeQuery(query);
+//            while (queryResult.next()) {
+//                System.out.println("id: " + queryResult.getInt(1));
+//                System.out.println("name: " + queryResult.getString(2));
+//            }
+//        } catch (SQLException exc) {
+//            System.out.println("got exception: " + exc);
+//            exc.printStackTrace();
+//        } finally {  // finally zawsze się wykona !!
+//            System.out.println("Finally i know it :) ");
+//            if (queryResult != null) {
+//                try {
+//                    queryStatement.close();
+//                } catch (SQLException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//            System.out.println("The end ");
+//        }
+//    }
+//}
