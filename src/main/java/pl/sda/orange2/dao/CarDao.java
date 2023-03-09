bedf06ff1d117d4918fb57775f6106e8e6f03066
Marta <marta.trebicka@wp.pl>
2023-03-09 18:03:56 +0100

    [change] - generic DataAccess dao created

--- src/main/java/pl/sda/orange2/dao/CarDao.java
+++ src/main/java/pl/sda/orange2/dao/CarDao.java
@@ -4,19 +4,21 @@ import pl.sda.orange2.entity.Car;
 
 import java.util.List;
 
-public class CarDao {
-
+public class CarDao implements  DataAccess <Car, Long>{
+@Override
     public void save(Car car) {
 
     }
-
+@Override
     public List<Car> findAll() { //metoda do wyszukiwania wszystkich obietków
    return List.of();
     }
+    @Override
     // metoda do szukania po nr id
     public Car findById (Long id) {
         return null;
     }
+    @Override
     public void deleteById(Long id) {
 
     }
